9efb71fed586f2b82f875bc46726ca728f4cb0f3
aadikatiwari <aadikatiwari15@gmail.com>
2026-07-08 19:54:20 +0530

    commit

--- src/App.tsx
+++ src/App.tsx
@@ -6,7 +6,7 @@ import SpecialOffers from "./Components/SpecialOffers/SpecialOffers";
 import Gallery from "./Components/Gallery/Gallery";
 import Testimonials from "./Components/Testimonials/Testimonials";
 import Stats from "./Components/Stats/Stats";
-import Reservation from "./Components/Reservation/Reservation";
+import Reservation from "./Components/Reservations/Reservations";
 import Contact from "./Components/Contact/Contact";
 import Newsletter from "./Components/Newsletter/Newsletter";
 import Footer from "./Components/Footer/Footer";
